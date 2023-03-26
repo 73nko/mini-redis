4ecdea9d25fd25dc189c7d4677c398e797853de2
Alex Perez <apramos89@gmail.com>
2023-03-26 20:30:45 +0200

    Handling timeouts and failed connexions

--- src/mini_redis/commands.rs
+++ src/mini_redis/commands.rs
@@ -1,43 +1,49 @@
-use crate::mini_redis::request::{parse_request, Command};
+use std::time::Duration;
 
-use super::{KeyValueStore, Request};
+use crate::mini_redis::request::{parse_request, Command, Request};
+
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufStream};
 use tokio::net::TcpStream;
 
-pub async fn handle_connection(mut socket: TcpStream, store: KeyValueStore) {
-    let mut buffer = String::new();
-    let mut buf = BufStream::new(&mut socket);
+use super::KeyValueStore;
 
+const CONNECTION_TIMEOUT: Duration = Duration::from_secs(10);
+
+pub async fn handle_connection(mut socket: TcpStream, store: KeyValueStore) {
+    let timeout_result = tokio::time::timeout(CONNECTION_TIMEOUT, async {
+        handle_request(&mut socket, &store).await
+    })
+    .await;
+
+    match timeout_result {
+        Ok(()) => {}
+        Err(err) => eprintln!("Error handling connection: {}", err),
+    }
+
+    let _ = &socket.shutdown().await;
+}
+
+async fn handle_request(socket: &mut TcpStream, store: &KeyValueStore) {
+    let mut buffer = String::new();
+    let mut buf = BufStream::new(socket);
     buf.read_line(&mut buffer).await.unwrap();
-    println!("Request: {}", buffer);
 
     match parse_request(&buffer) {
         Ok(request) => {
             println!("Parsed request: {:?}", request);
 
             match request.command {
-                Command::Get => {
-                    get_command_from_request(&store, &mut buf, request).await;
-                }
-                Command::Set => {
-                    set_command_from_request(&store, &mut buf, request).await;
-                }
-                Command::Delete => {
-                    delete_command_from_request(&store, &mut buf, request).await;
-                }
-                Command::Exists => {
-                    exists_command_from_request(&store, &mut buf, request).await;
-                }
-                Command::Keys => {
-                    keys_command_from_request(&store, &mut buf, request).await;
-                }
+                Command::Get => get_command_from_request(store, &mut buf, request).await,
+                Command::Set => set_command_from_request(store, &mut buf, request).await,
+                Command::Delete => delete_command_from_request(store, &mut buf, request).await,
+                Command::Exists => exists_command_from_request(store, &mut buf, request).await,
+                Command::Keys => keys_command_from_request(store, &mut buf, request).await,
             }
         }
         Err(e) => {
             eprintln!("Error parsing request: {}", e);
             let response = format!("Error parsing request: {}", e);
             buf.write_all(response.as_bytes()).await.unwrap();
-
             buf.flush().await.unwrap();
         }
     }
