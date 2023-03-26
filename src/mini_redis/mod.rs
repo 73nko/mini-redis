4e4bb031357299e15dbca765f3a9cba47e86afdf
Alex Perez <apramos89@gmail.com>
2023-03-26 12:10:31 +0200

    Organize code in modules

---
+++ src/mini_redis/mod.rs
@@ -0,0 +1,10 @@
+mod commands;
+mod request;
+
+pub use commands::handle_connection;
+pub use request::Request;
+use tokio::sync::Mutex;
+
+use std::{collections::HashMap, sync::Arc};
+
+pub type KeyValueStore = Arc<Mutex<HashMap<String, String>>>;
---
+++ src/mini_redis/request.rs
@@ -0,0 +1,28 @@
+use serde::Deserialize;
+use serde_json::Error;
+
+#[derive(Debug, Deserialize, PartialEq)]
+pub enum Command {
+    #[serde(alias = "GET")]
+    Get,
+    #[serde(alias = "SET")]
+    Set,
+    #[serde(alias = "DEL")]
+    Delete,
+    #[serde(alias = "EXISTS")]
+    Exists,
+    #[serde(alias = "KEYS")]
+    Keys,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Request {
+    pub command: Command,
+    pub key: Option<String>,
+    pub value: Option<String>,
+    pub pattern: Option<String>,
+}
+
+pub fn parse_request(request_data: &str) -> Result<Request, Error> {
+    serde_json::from_str(request_data)
+}
